e794b7b3b45d8d884f65e11edf145822de40d9f7
Haru88 <tfiedler1@studenten.hs-bremerhaven.de>
2020-06-30 13:20:47 +0200

    final2

---
+++ SETTINGS.js
@@ -0,0 +1,25 @@
+export function settings(){
+    return {
+        CURRENT_ID : "31",
+        DATE : "31.06.2020",
+        ASSET_URL : "findFiles.php?key=values.json&start=../assets/",
+        ITEM_URL : "findFiles.php?key=items&start=",
+        PAGEDATA_URL : "pageData.json",
+        ASSET_FILE : "mesh.glb"
+    }
+}
+
+
+/////////////////////////EXAMPLE/////DONT TOUCH THIS////////////////////////////////////////////////
+/*
+export function settings(){
+    return {
+        CURRENT_ID : "75",
+        DATE : "31.06.2020",
+        ASSET_URL : "findFiles.php?key=values.json&start=../assets/",
+        ITEM_URL : "findFiles.php?key=items&start=",
+        PAGEDATA_URL : "pageData.json",
+        ASSET_FILE : "mesh.glb"
+    }
+}
+*/
